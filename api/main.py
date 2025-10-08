41627903265f0999604cd3967e7f552eb2fbce70
Giulia Gargiulo <giulia.gargiulo3@studenti.unina.it>
2025-10-08 12:31:30 +0100

    created a new endcase that convert a csv file in a txt file

--- api/main.py
+++ api/main.py
@@ -12,9 +12,16 @@ def greet_user(name:str):
     return {"message" : f"Hello {name}, welcome to the Events Manager API!"}
 
 @app.post("/events/save-to-file")
-def save_event_to_file(event: models.CreateEvent):
+def save_event_to_file(event: models.Event):
     with open("../events.txt", "w") as f:
-        f.write(f"Event:{event.name} Date:{event.date} Location:{event.location} "
+        f.write(f"ID:{event.id} Event:{event.name} Date:{event.date} Location:{event.location} "
                 f"Start Time:{event.start_time} End Time:{event.end_time}\n")
         return {"message": "Event saved to file successfully."}
-    
+
+
+@app.post("/events/convert-file")
+def convert_csv_to_txt():
+    with open("../events.csv", "r") as csv_f, open("../events.txt", "a") as txt_f:
+        for line in csv_f:
+            txt_f.write(line.replace(",", " ")) 
+    return {"message": "CSV file converted to TXT successfully."}
