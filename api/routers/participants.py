31088bfda04fa5566784bf66fb0a63ca250d1d71
Giulia Gargiulo <giulia.gargiulo3@studenti.unina.it>
2025-10-10 16:58:27 +0100

    Added description for participants router endpoints

--- api/routers/participants.py
+++ api/routers/participants.py
@@ -1,3 +1,4 @@
+from ast import List
 from fastapi import APIRouter, Depends
 from api import classes
 from api.database import get_db
@@ -9,18 +10,29 @@ router = APIRouter(prefix="/participants", tags=["participants"])
 # CRUD Operations for Participants
 # CREATE
 
-@router.post("/")
+@router.post("/",
+             summary="Create a Participant",
+             description="Create a new participant in the database",
+             response_description="A message indicating the result of the creation operation")
 async def create_participant(participant: classes.Participant, db: Connection = Depends(get_db)):
     query = "INSERT INTO participants (first_name, last_name, email, phone) VALUES ($1, $2, $3, $4) RETURNING id;"
     row = await db.fetchrow(query, participant.first_name, participant.last_name, participant.email, participant.phone)
     return {"message": "Participant created successfully", "id": row['id']}
     
 # READ
-@router.get("/")
+@router.get("/",
+             summary="Get All Participants",
+             description="Retrieve all participants from the database",
+             response_description="A list of participants in JSON format",
+             response_model=List[classes.Participant])
 async def get_participants(db: Connection = Depends(get_db)):
     return await db.fetch("SELECT * FROM participants;")
 
-@router.get("/{participant_id}")
+@router.get("/{participant_id}",
+            summary="Get Participant by ID",
+            description="Retrieve a specific participant by its ID from the database",
+            response_description="A participant in JSON format",
+            response_model=classes.Participant)
 async def get_participant(participant_id: int, db: Connection = Depends(get_db)):
     participant = await db.fetchrow("SELECT * FROM participants WHERE id = $1", participant_id)
     if not participant:
@@ -28,18 +40,23 @@ async def get_participant(participant_id: int, db: Connection = Depends(get_db))
     return participant
 
 # UPDATE
-@router.put("/{participant_id}")
+@router.put("/{participant_id}",
+            summary="Update a Participant",
+            description="Update an existing participant in the database",
+            response_description="A message indicating the result of the update operation")
 async def update_participant(participant_id: int, updated_participant: classes.Participant, db: Connection = Depends(get_db)):
     query = """
         UPDATE participants SET first_name = $1, last_name = $2, email = $3, phone = $4 WHERE id = $5
     """
     values = (updated_participant.first_name, updated_participant.last_name, updated_participant.email, updated_participant.phone, participant_id)
     row = await db.fetchrow(query, *values)
-    print({"message": "Participant updated successfully"})
-    return dict(row)
+    return {"message": "Participant updated successfully"}
 
 # DELETE
-@router.delete("/{participant_id}")
+@router.delete("/{participant_id}",
+               summary="Delete a Participant",
+               description="Delete a participant from the database",
+               response_description="A message indicating the result of the deletion operation")
 async def delete_participant(participant_id: int, db: Connection = Depends(get_db)):
     query = "DELETE FROM participants WHERE id = $1"
     await db.execute(query, (participant_id,))
