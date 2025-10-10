34dfc1a858ee5a4faef4e1a5eab761e8206270c0
Giulia Gargiulo <giulia.gargiulo3@studenti.unina.it>
2025-10-10 16:55:30 +0100

    Added description for events router endpoints

--- api/routers/base.py
+++ api/routers/base.py
@@ -16,3 +16,16 @@ def root():
             response_description="A personalised welcome message in JSON format")
 def greet_user(name:str):
     return {"message" : f"Hello {name}, welcome to the Events Manager API!"}
+
+# convert CSV to TXT
+@router.post("/convert-file",
+             summary="Convert CSV to TXT",
+             description="Convert a CSV file to a TXT file",
+             response_description="A message indicating the result of the conversion operation")
+def convert_csv_to_txt():
+    csv_file_path = os.path.join(FILES_DIR, "events.csv")
+    txt_file_path = os.path.join(FILES_DIR, "events.txt")
+    with open(csv_file_path, "r") as csv_f, open(txt_file_path, "a") as txt_f:
+        for line in csv_f:
+            txt_f.write(line.replace(",", " "))
+    return {"message": "CSV file converted to TXT successfully."}
--- api/routers/events.py
+++ api/routers/events.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends
 import os
 from api import classes
@@ -12,7 +13,10 @@ FILES_DIR = os.path.join(BASE_DIR, "files")
 
 # Save an event on a text file
 
-@router.post("/save-to-file")
+@router.post("/save-to-file",
+             summary="Save Event to File",
+             description="Save a new event in a text file",
+             response_description="A message indicating the result of the save operation")
 def save_event_to_file(event: classes.Event):
     file_path = os.path.join(FILES_DIR, "events.txt")
     with open(file_path, "a") as f:
@@ -20,21 +24,13 @@ def save_event_to_file(event: classes.Event):
                 f"Start Time:{event.start_time} End Time:{event.end_time}\n")
         return {"message": "Event saved to file successfully."}
 
-# convert CSV to TXT
-
-@router.post("/convert-file")
-def convert_csv_to_txt():
-    csv_file_path = os.path.join(FILES_DIR, "events.csv")
-    txt_file_path = os.path.join(FILES_DIR, "events.txt")
-    with open(csv_file_path, "r") as csv_f, open(txt_file_path, "a") as txt_f:
-        for line in csv_f:
-            txt_f.write(line.replace(",", " "))
-    return {"message": "CSV file converted to TXT successfully."}
-
 # CRUD Operations for Events
 # CREATE
 
-@router.post("/")
+@router.post("/",
+             summary="Create an Event",
+             description="Create a new event in the database",
+             response_description="A message indicating the result of the creation operation")
 async def create_event(event: classes.Event, db: Connection = Depends(get_db)):
     query = "INSERT INTO events (name, event_date, location, start_time, end_time) VALUES ($1, $2, $3, $4, $5) RETURNING id;"
     row = await db.fetchrow(query, event.name, event.event_date, event.location, event.start_time, event.end_time)
@@ -42,12 +38,20 @@ async def create_event(event: classes.Event, db: Connection = Depends(get_db)):
 
 # READ
 
-@router.get("/")
+@router.get("/",
+             summary="Get All Events",
+             description="Retrieve all events from the database",
+             response_description="A list of events in JSON format",
+             response_model=List[classes.Event])
 async def get_events(db: Connection = Depends(get_db)):
     return await db.fetch("SELECT * FROM events;")
 
 
-@router.get("/{event_id}")
+@router.get("/{event_id}",
+             summary="Get Event by ID",
+             description="Retrieve a specific event by its ID from the database",
+             response_description="An event in JSON format",
+             response_model=classes.Event)
 async def get_event(event_id: int, db: Connection = Depends(get_db)):
     event = await db.fetchrow("SELECT * FROM events WHERE id = $1", event_id)
     if not event:
@@ -56,7 +60,10 @@ async def get_event(event_id: int, db: Connection = Depends(get_db)):
     
 # UPDATE
 
-@router.put("/{event_id}")
+@router.put("/{event_id}",
+             summary="Update an Event",
+             description="Update an existing event in the database",
+             response_description="A message indicating the result of the update operation")
 async def update_event(event_id: int, event: classes.Event, db: Connection = Depends(get_db)):
     db_event = await db.fetchrow("SELECT * FROM events WHERE id = $1", event_id)
     if not db_event:
@@ -71,7 +78,10 @@ async def update_event(event_id: int, event: classes.Event, db: Connection = Dep
 
 # DELETE
 
-@router.delete("/{event_id}")
+@router.delete("/{event_id}",
+             summary="Delete an Event",
+             description="Delete an event from the database",
+             response_description="A message indicating the result of the deletion operation")
 async def delete_event(event_id: int, db: Connection = Depends(get_db)):
     query = "DELETE FROM events WHERE id = $1"
     await db.execute(query, (event_id,))
